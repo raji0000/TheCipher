fc0469b14797114a93f15dd04871aebffb285356
raji0000 <158059604+raji0000@users.noreply.github.com>
2024-05-30 13:57:41 +0545

    Add files via upload

---
+++ thecode.py
@@ -0,0 +1,49 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+from asci import logo
+print(logo)
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if  cipher_direction == "decode":
+            shift_amount = shift_amount * -1 
+    
+    for char in start_text:
+        if  char in alphabet:
+             position = alphabet.index(char)
+             new_position = position + shift_amount 
+             end_text = end_text + alphabet[new_position]
+        else:
+             end_text = end_text + char
+             
+    
+    print(f"The {cipher_direction}text  is {end_text}\n")
+
+
+should_continue = True
+while should_continue:
+     direction = input("Type 'encode' to encrypt, type 'decode'to decrypt:\n")
+     text = input("Type your message:\n").lower()
+     shift = int(input("Enter the shift number:\n"))
+
+     shift = shift % 26
+     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+     result = input("Type 'yes' if you want to do agina. Otherwise type 'no'.\n")
+
+     
+     if result == "no":
+          should_continue = False
+          print("GoodBye")
+    
+
+    
+     
+    
+    
+
+    
+
+                            
+        
+            
